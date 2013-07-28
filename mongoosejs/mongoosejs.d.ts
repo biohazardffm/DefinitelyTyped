e2b9c4edc309a7ce86b312185155c7210e5b06dd
Danny Thuering <dthuering@gmail.com>
2013-07-28 22:43:19 +0200

    try mongoose

--- mongoosejs/mongoosejs.d.ts
+++ mongoosejs/mongoosejs.d.ts
@@ -2,44 +2,167 @@
 
 declare module "mongoose" {
 
-	export interface Mongoose {
+    export interface Mongoose {
+        constructor();
+        set (key: string, value: string): Mongoose;
+        get (key: string): string;
+        createConnection(uri?: string, options?: any): Connection;
 
-	}
+        connect(any): Mongoose;
 
-    export function connect(any): Mongoose;
-	export function model(name: string, schema?: Schema, collection?: string, skipInit?: bool): Model;
-
-	export class Schema {
-		constructor(definition:any, options?:any);
-		static Types: {
-			ObjectId: any;
-			Mixed: any;
-		};
-
-		methods: any;
-		statics: any;
-		path(path: string): any;
-		virtual(path: string): any;
-		pre(method: string, callback: (next: (any?) => any) => any): void;
-	}
-
-	export class Query {
-
-	}
-
-	export interface Model {
-		new (any): Document;
-
-		find(conditions: any): Query;
-		find(conditions: any, callback: (err: any, res: any[]) => void ): Query;
-	}
-
-    export interface Document {
-		_id: string;
-		update(doc: any, options: any, callback: (err: any, affectedRows: number, raw: any) => void ): Query;
-		save(fn?: (err: any, res: any) => void ): void;
-		remove(callback?: (err) => void ): Query;
+        disconnect(fn: (err?: any) => void ): Mongoose;
+        model(name: string, schema?: Schema, collection?: string, skipInit?: bool): Model;
+        modelNames(): string[];
+        plugin(fn: (any) => any, opts?: any): Mongoose;
+        mongo: any;
+        version: string;
+        connection: Connection;
     }
 
+    export function set(key: string, value: string): Mongoose;
+    export function get(key: string): string;
+    export function createConnection(uri ? : string, options?: any): Connection;
 
+    export function connect(any): Mongoose;
+
+    export function disconnect(fn: (err?: any) => void ): Mongoose;
+    export function model(name: string, schema?: Schema, collection?: string, skipInit?: bool): Model;
+    export function modelNames(): string[];
+    export function plugin(fn: (any) => any, opts?: any): Mongoose;
+    export var mongo: any;
+    export var version: string;
+    export var connection: Connection;
+    
+    export class Collection {
+        name: string;
+    }
+
+    export class Connection implements EventEmitter {
+        constructor(base: Mongoose);
+
+        addListener(event: string, listener: Function);
+        on(event: string, listener: Function);
+        once(event: string, listener: Function): void;
+        removeListener(event: string, listener: Function): void;
+        removeAllListeners(event: string): void;
+        setMaxListeners(n: number): void;
+        listeners(event: string): { Function; }[];
+        emit(event: string, ...args: any[]): void;
+
+        open(connection_string: string,
+             database?: string,
+             port?: number,
+             options?: any,
+             callback?: (any) => any): Connection;
+
+        openSet(uris: string,
+                database?: string,
+                options?: any,
+                callback?: (any) => any): Connection;
+
+        close(callback?: (any) => any): Connection;
+        collection(name: string, options?: any): Collection;
+        model(name: string, schema?: Schema, collection?: string): Model;
+        modelNames(): string[];
+        setProfiling(level: number, ms: number, callback: (any) => any): any;
+        db: any;
+        collections: any;
+        readyState: number;
+    }
+
+    export class Schema {
+        constructor(definition: any, options?: any);
+        static Types: {
+            ObjectId: any;
+            Mixed: any;
+        };
+
+        methods: any;
+        statics: any;
+        path(path: string): any;
+        virtual(path: string): any;
+        pre(method: string, callback: (next: (any?) => any) => any): void;
+    }
+
+    export class SchemaType { }
+
+    export class VirtualType { }
+
+    /**
+     * Type param T should be descendant of Document
+     */
+    export class Query<T> {
+        exec(): Promise;
+        exec(operation: string): Promise;
+        exec(callback: (err: any, res: T[]) => any): Promise;
+        exec(operation: string, callback: (err: any, res: T[]) => void ): Promise;
+
+        skip(x: number): Query;
+        limit(x: number): Query;
+    }
+
+    export class Promise { }
+
+    /**
+     * Type param T should be descendant of Document
+     */
+    export interface Model<T> {
+        new (any): Document;
+
+        find(conditions: any): Query<T>;
+        find(conditions: any, fields: any): Query<T>;
+        find(conditions: any, fields: any, options: any): Query<T>;
+        find(conditions: any, fields: any, options: any, callback: (err: any, res: any) => void ): Query<T>;
+        find(conditions: any, callback: (err: any, res: T[]) => void ): Query<T>;
+        find(conditions: any, fields: any, callback: (err: any, res: T[]) => void ): Query<T>;
+
+        findOne(conditions: any): Query<T>;
+        findOne(conditions: any, fields: any): Query<T>;
+        findOne(conditions: any, fields: any, options: any): Query<T>;
+        findOne(conditions: any, fields: any, options: any, callback: (err: any, res: T) => void ): Query<T>;
+        findOne(conditions: any, callback: (err: any, res: T) => void ): Query<T>;
+        findOne(conditions: any, fields: any, callback: (err: any, res: T) => void ): Query<T>;
+
+        findById(id: string): Query<T>;
+        findById(id: string, fields: any): Query<T>;
+        findById(id: string, fields: any, options: any): Query<T>;
+        findById(id: string, fields: any, options: any, callback: (err: any, res: T) => void ): Query<T>;
+        findById(id: string, callback: (err: any, res: T) => void ): Query<T>;
+        findById(id: string, fields: any, callback: (err: any, res: T) => void ): Query<T>;
+
+        findByIdAndUpdate(id: string): Query<T>;
+        findByIdAndUpdate(id: string, update: any): Query<T>;
+        findByIdAndUpdate(id: string, update: any, options: any): Query<T>;
+        findByIdAndUpdate(id: string, update: any, options: any, callback: (err: any, res: T[]) => void ): Query<T>;
+        findByIdAndUpdate(id: string, callback: (err: any, res: T[]) => void ): Query<T>;
+        findByIdAndUpdate(id: string, update: any, callback: (err: any, res: T[]) => void ): Query<T>;
+
+        update(conditions: any,
+               update: any,
+               options?: any,
+               callback?: (err: any, affectedRows: number, raw: any) => void ): Query<T>;
+        update(conditions: any,
+               update: any,
+               callback?: (err: any, affectedRows: number, raw: any) => void ): Query<T>;
+
+        create(doc: any, fn: (err: any, res: T) => void ): void;
+
+        collection: Collection;
+
+        remove(conditions: any, callback?: (err) => void): Query<T>;
+    }
+
+    export interface Document {
+        _id: string;
+        update<T>(doc: any, options: any, callback: (err: any, affectedRows: number, raw: any) => void ): Query<T>;
+        save<T>(fn?: (err: any, res: T) => void ): void;
+        remove<T>(callback?: (err) => void ): Query<T>;
+    }
+
+    export class MongooseError { }
+
+    export class Types { }
+
+    export class SchemaTypes { }
 }
+
